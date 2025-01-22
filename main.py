6bb8c040e341808773eace870daa3e1b2790b9d5
PRAISE <dcpraise24@gmail.com>
2025-01-22 19:55:44 +0100

    added a new feature

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-
+"""
+Feature 1.
+this first feature is a random number guess game.
+"""
 import random
 
 def make_a_guess():
     """this function generates a random number 0-100 and ask the player to guess the number, if the player's guess is lower or higher than the number the game continues, but if the player's guess is correct the game exits automatically. the player can also enter q to quit the game.
     """
     number = random.randint(0, 100)
-
 
     while True:
         guess = input("Guess the Number [q]: ")
@@ -32,4 +34,61 @@ def make_a_guess():
             print(f"{guess} is CORRECT!")
             break
 
-make_a_guess()
+# make_a_guess()
+
+
+"""
+ Feature 2.
+ this second feature is a compound interest calculator 
+"""
+
+principal = 0
+rate = 0
+time = 0
+is_true = True
+
+
+'''this function is to catch any ValueError exception from the user input'''
+def is_valid(num, value="float"):
+    try:
+        float(num)
+    except ValueError:
+        print(f"{num} is Not valid!")
+    else:
+        if value == "time":
+            return int(num)
+        else: return  float(num)
+
+    return
+
+
+while is_true:
+        principal = is_valid(input("Enter the principal amount: "))
+        if principal is not  None:
+            if principal <= 0:
+                print(f"principal cannot be less than or equal to zero")
+            else:
+                break
+
+
+while is_true:
+    rate = is_valid(input("Enter the interest rate: "))
+    if rate is not  None:
+        if rate <= 0:
+            print(f"rate cannot be less than or equal to zero")
+        else:
+            break
+
+
+while is_true:
+    time = is_valid(input("Enter the time in years: "), "time")
+    if time is not None:
+        if time <= 0:
+            print(f"time cannot be less than or equal to zero")
+        else:
+            break
+
+
+total = principal * pow((1 + rate / 100), time)
+
+print(f"Balance after {time} year/s is: ${total:.2f}")
